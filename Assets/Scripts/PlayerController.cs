e8defc19acc8cc63e395ae71444ed9726c6c4b03
ETHERXGAMES <etherxgames.official@gmail.com>
2022-11-27 13:17:03 +0200

    Bug Fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,10 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer;
     public Rigidbody2D rb;
     float move;
-    float playerSpeed = 30f, jumpPower = 35f;
+    public float playerSpeed = 30f;
+    public float jumpPower = 40f;
+
+    public bool isgrounded;
 
     void Start()
     {
@@ -18,6 +21,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         move = Input.GetAxis("Horizontal");
+        isgrounded = isGrounded();
 
         if(isGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
